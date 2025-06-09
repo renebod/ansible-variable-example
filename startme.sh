55e2d82016be42ac4a46b83b0812bda32a0e325f
René Bod <rene.bod@mac.com>
2025-06-09 13:51:45 +0200

    Final fixes

--- startme.sh
+++ startme.sh
@@ -2,3 +2,6 @@
 
 # docker-compose run --rm jupyter python3 create_workshop.py
 # docker-compose -f workshop_python_edition_01.yml up -d
+
+docker-compose run --rm ubuntu neofetch
+docker-compose run --rm ubuntu bash
