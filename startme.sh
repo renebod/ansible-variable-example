ef903ed0bc56d1625e7393c1cb631c544d551b83
René Bod <rene.bod@mac.com>
2025-06-09 13:34:43 +0200

    sshpass added

--- startme.sh
+++ startme.sh
@@ -2,5 +2,3 @@
 
 # docker-compose run --rm jupyter python3 create_workshop.py
 # docker-compose -f workshop_python_edition_01.yml up -d
-
-docker compose run --rm jupyter ssh -o StrictHostKeyChecking=no alice@ubuntu "getent passwd alice"
