b973ddab2b07d6a10996192170cb6db27938b63b
René Bod <rene.bod@mac.com>
2025-06-09 12:56:55 +0200

    Launcher

--- startme.sh
+++ startme.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
-docker-compose run --rm jupyter python3 create_workshop.py
+# docker-compose run --rm jupyter python3 create_workshop.py
 # docker-compose -f workshop_python_edition_01.yml up -d
+
+docker compose run --rm jupyter ssh -o StrictHostKeyChecking=no alice@ubuntu "getent passwd alice"
